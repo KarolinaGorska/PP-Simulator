7812b54af2aeee5ef59bbfaeed8ae2653b123638
KarolinaGorska <231712@student.uek.krakow.pl>
2024-10-28 22:54:08 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,13 @@
+﻿namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+
+    public uint Size { get; set; } = 3;
+
+    public void Info()
+    {
+        Console.WriteLine($"{Description} <{Size}>");
+    }
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,29 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string? Name { get; }
+
+    private int level;
+
+    public int Level
+    {
+        get => level;
+        set => level = value > 0 ? value : 1;
+    }
+
+    public Creature(string name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+       
+    }
+
+    public string Info => $"{Name} [{level}]";
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+}
