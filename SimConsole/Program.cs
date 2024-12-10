8cfee0d3b5f668d9284f2b834b658ad0199825e0
KarolinaGorska <231712@student.uek.krakow.pl>
2024-12-10 22:38:13 +0100

    Duża mapa

--- SimConsole/Program.cs
+++ SimConsole/Program.cs
@@ -6,19 +6,38 @@ using Point = Simulator.Point;
 namespace SimConsole
 {
     internal class Program
-    { 
+    {
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Starting Simulator!\n");
             Console.WriteLine("Starting positions");
-            SmallTorusMap map = new(8, 6);
-            List<IMappable> mappables = new() { new Orc("Gorbag"), new Elf("Elandor"),
-            new Animals("Kroliki", 5), new Birds("Orly", 11, true), new Birds("Strusie", 4, false) };
-            List<Point> points = new() { new(2, 2), new(3, 1), new(4, 4), new(2, 5), new(0, 0) };
-            string moves = "drulldrudldrlul";
+
+            BigBounceMap map = new(8, 6);
+
+            List<IMappable> mappables = new()
+            {
+                new Orc("Gorbag"),
+                new Elf("Elandor"),
+                new Animals("Kroliki", 5),
+                new Birds("Orly", 11, true),
+                new Birds("Strusie", 4, false)
+            };
+
+            List<Point> points = new()
+            {
+                new(2, 2),
+                new(3, 1),
+                new(4, 4),
+                new(2, 5),
+                new(0, 0)
+            };
+
+            string moves = "drulldrudlddrluldrulldruulldlu";
+
             Simulation simulation = new(map, mappables, points, moves);
             MapVisualizer mapVisualizer = new(simulation.Map);
+
             var move = 1;
             mapVisualizer.Draw();
             while (!simulation.Finished)
@@ -31,7 +50,6 @@ namespace SimConsole
                 simulation.Turn();
                 mapVisualizer.Draw();
                 move++;
-
             }
         }
     }
---
+++ Simulator/Maps/BigBounceMap.cs
@@ -0,0 +1,100 @@
+﻿namespace Simulator.Maps
+{
+    public class BigBounceMap : BigMap
+    {
+        public BigBounceMap(int sizeX, int sizeY) : base(sizeX, sizeY) { }
+
+        public override Point Next(Point p, Direction d)
+        {
+            Point nextPoint;
+
+            switch (d)
+            {
+                case Direction.Up:
+                    nextPoint = new Point(p.X, p.Y + 1);
+                    if (nextPoint.Y >= SizeY)
+                    {
+                        nextPoint = new Point(p.X, p.Y - 1);
+                    }
+                    break;
+                case Direction.Down:
+                    nextPoint = new Point(p.X, p.Y - 1);
+                    if (nextPoint.Y < 0)
+                    {
+                        nextPoint = new Point(p.X, p.Y + 1);
+                    }
+                    break;
+                case Direction.Left:
+                    nextPoint = new Point(p.X - 1, p.Y);
+                    if (nextPoint.X < 0)
+                    {
+                        nextPoint = new Point(p.X + 1, p.Y);
+                    }
+                    break;
+                case Direction.Right:
+                    nextPoint = new Point(p.X + 1, p.Y);
+                    if (nextPoint.X >= SizeX)
+                    {
+                        nextPoint = new Point(p.X - 1, p.Y);
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Nieznany kierunek");
+            }
+
+            // Sprawdzamy, czy obiekt nadal wykracza poza mapę po odbiciu
+            if (nextPoint.X < 0 || nextPoint.X >= SizeX || nextPoint.Y < 0 || nextPoint.Y >= SizeY)
+            {
+                return p;
+            }
+
+            return nextPoint;
+        }
+
+        public override Point NextDiagonal(Point p, Direction d)
+        {
+            Point nextPoint;
+
+            switch (d)
+            {
+                case Direction.Up:
+                    nextPoint = new Point(p.X + 1, p.Y + 1);
+                    if (nextPoint.X >= SizeX || nextPoint.Y >= SizeY)
+                    {
+                        nextPoint = new Point(p.X - 1, p.Y - 1);
+                    }
+                    break;
+                case Direction.Down:
+                    nextPoint = new Point(p.X - 1, p.Y - 1);
+                    if (nextPoint.X < 0 || nextPoint.Y < 0)
+                    {
+                        nextPoint = new Point(p.X + 1, p.Y + 1);
+                    }
+                    break;
+                case Direction.Left:
+                    nextPoint = new Point(p.X - 1, p.Y + 1);
+                    if (nextPoint.X < 0 || nextPoint.Y >= SizeY)
+                    {
+                        nextPoint = new Point(p.X + 1, p.Y - 1);
+                    }
+                    break;
+                case Direction.Right:
+                    nextPoint = new Point(p.X + 1, p.Y - 1);
+                    if (nextPoint.X >= SizeX || nextPoint.Y < 0)
+                    {
+                        nextPoint = new Point(p.X - 1, p.Y + 1);
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Nieznany kierunek");
+            }
+
+            if (nextPoint.X < 0 || nextPoint.X >= SizeX || nextPoint.Y < 0 || nextPoint.Y >= SizeY)
+            {
+                return p;
+            }
+
+            return nextPoint;
+        }
+    }
+}
